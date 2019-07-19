80e54ec1c2958eacfc4b8d7a3e71772c5f0337ed
Kestutis Andriunas <kendriunas@gmail.com>
2019-07-19 09:46:18 +0300

    Homework

---
+++ src/lt/bit/second/day/Homework.java
@@ -0,0 +1,9 @@
+package lt.bit.second.day;
+
+public class Homework {
+    public static void main(String[] args) {
+    int numberOne = 2;
+    int numberTwo = 3;
+        System.out.println( numberOne + numberTwo );
+    }
+}
